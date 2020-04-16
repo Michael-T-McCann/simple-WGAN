86806fe95ed6ae23af7f632aab6073d3f091a215
Michael McCann <michael.thompson.mccann@gmail.com>
2020-04-16 16:25:52 -0400

    Nice results on denoising and inpainting

--- generator.py
+++ generator.py
@@ -3,11 +3,10 @@ import torch
 import torchvision
 
 class NoisyImage(torch.nn.Module):
-    def __init__(self, size=None, sigma=0.0, x0=None, do_crop=False):
+    def __init__(self, size=None, sigma=0.0, x0=None, do_masking=False):
         super().__init__()
 
-        self.crop_size = 256
-        self.do_crop = do_crop
+        self.do_masking = do_masking
 
         self.sigma = sigma
         
@@ -15,13 +14,14 @@ class NoisyImage(torch.nn.Module):
             self.x = torch.nn.Parameter(torch.zeros(size))
         else:
             self.x = torch.nn.Parameter(torch.tensor(x0, dtype=torch.float))
-
+                
     def forward(self):
+        self.zero = torch.zeros_like(self.x)  # todo: make once, put on GPU
         x = self.x
+        if self.do_masking:
+            mask = torch.rand_like(x) > 0.5
+            x = torch.where(mask, x, self.zero)
+
         x = x + self.sigma * torch.randn_like(x)
-        if self.do_crop:
-            m = np.random.randint(0, self.x.shape[0]-self.crop_size)
-            n = np.random.randint(0, self.x.shape[1]-self.crop_size)
-            x = x[m:m+self.crop_size, n:n+self.crop_size]
                 
         return x
--- main.py
+++ main.py
@@ -8,63 +8,63 @@ import os
 import dataset, generator, discriminator
 # params
 
-do_crop = True
-crop_size = 256
+do_masking = True
 
-
-sigma = 0.2
-
-if not do_crop:
-    num_steps = 20
+if not do_masking:
+    sigma = 0.5
+    num_steps = 40
     num_steps_G = 3
     num_steps_D = 3
 
-    batch_size = 2
+    batch_size = 3
     learning_rate_G = 1.0e-2
     learning_rate_D = 1.0e-2
 
     reg_weight = 1.0e1
+
+    output_step = 5
 else:
+    sigma = 0.5
     num_steps = 500
     num_steps_G = 3
-    num_steps_D = 20
+    num_steps_D = 3
 
-    batch_size = 2
-    learning_rate_G = 1.0e-2
-    learning_rate_D = 1.0e-2
+    batch_size = 3
+    learning_rate_G = 1.0e-3
+    learning_rate_D = 1.0e-3
 
-    reg_weight = 1.0e1
-
+    reg_weight = 1.0e0
+    output_step = 10
+    
 device = torch.device('cuda')
 
 # setup output folder
 outdir = 'results_' + time.strftime("%Y%m%d-%H%M%S")
 os.makedirs(outdir)
 
+# setup plots
+plt.set_cmap('gray')
+
 # true data distribution
 im = scipy.misc.face()
 im = np.mean(im/255, axis=2)
-if not do_crop:
-    im = im[100:100+256, 400:400+256]
+im = im[100:100+256, 400:400+256]
 im = im - np.mean(im)
 
-G_true = generator.NoisyImage(sigma=sigma, x0=im, do_crop=do_crop)
+G_true = generator.NoisyImage(sigma=sigma, x0=im, do_masking=do_masking)
 G_true.requires_grad_(False)
 G_true = G_true.to(device)
 data_true = iter(torch.utils.data.DataLoader(
     dataset.GeneratorWrapper(G_true), batch_size=batch_size))
 
 # generator
-G = generator.NoisyImage(size=im.shape, sigma=sigma, do_crop=do_crop)
+G = generator.NoisyImage(size=im.shape, sigma=sigma, do_masking=do_masking)
 G = G.to(device)
 data_fake = iter(torch.utils.data.DataLoader(
     dataset.GeneratorWrapper(G), batch_size=batch_size))
 
 # discriminator
-if not do_crop:
-    D = discriminator.ConvMax(im.size)
-else:
-    D = discriminator.ConvMax(crop_size**2)
+D = discriminator.ConvMax(im.size)
     
 D = D.to(device)
 
@@ -99,26 +99,36 @@ for step in range(num_steps):
         score_true = torch.mean(D(x_true))
         score_fake = torch.mean(D(x_fake))
 
-        loss_D_reg = -score_true + score_fake
-        loss_D = loss_D_reg.detach()
+        loss_D = -score_true + score_fake
+
+        optim_D.zero_grad()
+        loss_D.backward()
+        optim_D.step()
 
         # regularization
         with torch.no_grad():
-            alpha = torch.rand(batch_size, device=device)
-            alpha = alpha.unsqueeze(1).unsqueeze(2)
-            x_between = alpha*x_true + (1-alpha)*x_fake
+             alpha = torch.rand(batch_size, device=device)
+             alpha = alpha.unsqueeze(1).unsqueeze(2)
+             x_between = alpha*x_true + (1-alpha)*x_fake
 
-        for i in range(batch_size):
-            x = x_between[i:i+1]
-            x.requires_grad_(True)
-            D(x).backward(create_graph=True)
-            reg = (torch.norm(x.grad)-1)**2
-            loss_D_reg = loss_D_reg + reg_weight/batch_size * reg
-        
+        # using backward leaks memory (I think),
+        # so we use grad instead
+        # see https://github.com/pytorch/pytorch/issues/4661
+        x_between.requires_grad_(True)
+        Dx = D(x_between)
+        grad_x = torch.autograd.grad(
+            Dx, x_between, torch.ones_like(Dx), create_graph=True)[0]
+        reg_D = reg_weight * torch.mean(
+            (
+                torch.sqrt(torch.sum(grad_x**2, axis=(1,2)))
+                -1)**2
+        )
         optim_D.zero_grad()
-        loss_D_reg.backward()
-        optim_D.step()
+        reg_D.backward()
+        optim_D.step()     
+             
 
+    
     # print loss, make plots
     with torch.no_grad():
         print("%10d\t%10.3e\t%10.3e\t%10.3e\t%10.3e" %
@@ -126,16 +136,29 @@ for step in range(num_steps):
                loss_fcn(G.x, G_true.x).item(),
                loss_G.item(),
                loss_D.item(),
-               loss_D_reg.item()
+               reg_D.item()
                ))
 
-        im_hat = G.x.detach().cpu().squeeze()
-        fig, ax = plt.subplots(1, 2)
-        ax[0].imshow(im)
+        if step % output_step != 0:
+            continue
+        
+        im_hat = G.x.detach().cpu().squeeze().numpy()
+        vmin, vmax = im.min(), im.max()
+        
+        fig, ax = plt.subplots(1, 3)
+        im_h = ax[0].imshow(im, vmin=vmin, vmax=vmax)
         ax[0].set_title('ground truth')
-        ax[1].imshow(im_hat)
+        fig.colorbar(im_h, ax=ax[0])
+        
+        im_h = ax[1].imshow(im_hat, vmin=vmin, vmax=vmax)
         ax[1].set_title('reconstruction')
-        fig.savefig(os.path.join(outdir,f'im_{step}.png'))
+        fig.colorbar(im_h, ax=ax[1])
+
+        im_h = ax[2].imshow(np.abs(im-im_hat))
+        ax[2].set_title('absolute error')
+        fig.colorbar(im_h, ax=ax[2])
+
+        fig.savefig(os.path.join(outdir,f'im_{step}.png'))    
         plt.close(fig)
 
         fig, ax = plt.subplots(2, batch_size, squeeze=False)
