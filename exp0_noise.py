0df04e26761b6b6fab13301f3a1b383db0e3bf39
Muhammad Huwio <moehuwio@gmail.com>
2020-06-23 23:33:27 -0400

    Change Test Values

--- exp0_noise.py
+++ exp0_noise.py
@@ -84,10 +84,12 @@ def SweepThru(noiseSigma = 0.4,ns = 100, bs = 3, lr_G = 1e-2, lr_D = 1e-2, ns_G 
 
 
 #noise_sigmas = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]  
-num_steps_dis = [1,2,3,4,5,6,7,8,9,10]
+#num_steps_dis = [1,2,3,4,5,6,7,8,9,10]
+num_steps_gen = [1,2,3,4,5,6,7,8,9,10] 
+
 t = time.strftime("%Y%m%d-%H%M%S")
 
-os.makedirs("results/" + t + "D_step_Trials")
+os.makedirs("results/" + t + "G_step_Trials")
 
-for D_step in num_steps_dis: 
-    SweepThru(ns_D = D_step, path = (t+"D_step_Trials")) 
+for G_step in num_steps_gen:  
+    SweepThru(ns_G = G_step, path = (t+"G_step_Trials")) 
