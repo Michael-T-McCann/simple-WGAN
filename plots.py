79ad6b103ed5e6a898ffdb77f080dc9f3223a474
Muhammad Huwio <moehuwio@gmail.com>
2020-07-16 16:07:16 -0400

    Add plots.py

---
+++ plots.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+def PlotRes(outdir):
+    data = pd.read_csv(outdir+'/res.txt', sep="\t", header=None)  
+    MSE_xGT = data[1]  
+    MSE_xGT = MSE_xGT[1:]
+    MSE_xGT = MSE_xGT.to_numpy()
+    MSE_xGT = MSE_xGT.astype(float)  
+
+    MSE_AVG = data[6]  
+    MSE_AVG = MSE_AVG[1:]
+    MSE_AVG = MSE_AVG.to_numpy()
+    MSE_AVG = MSE_AVG.astype(float) 
+    MSE_AVG[0] = 0.0
+
+    
+    plt.plot(np.arange(100),MSE_xGT, label = 'MSE: x-x_GT')
+    plt.plot(np.arange(100),MSE_AVG, label = 'MSE: Avgeraging')  
+
+    plt.savefig(outdir + '/ResultGraph')
