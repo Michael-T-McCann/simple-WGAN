0369699e893030c77629032b386e0bfccdd141ce
Muhammad Huwio <moehuwio@gmail.com>
2020-07-17 09:59:39 -0400

    Updtate plots.py

--- plots.py
+++ plots.py
@@ -6,28 +6,23 @@ import sys
 def PlotRes(path):
     sys.stdout.close
 
-    f = open(path + '/res.txt') 
-    print(f.read()) 
-    f.close() 
-    
-    print("\nThis is the path that should be working but its not :(\t\t\t", path)
+    data = pd.read_csv(path + '/res.txt', sep="\t", header=None)  
+    MSE_xGT = data[1]  
+    MSE_xGT = MSE_xGT[1:]
+    MSE_xGT = MSE_xGT.to_numpy()
+    MSE_xGT = MSE_xGT.astype(float)
 
-    alsoData = np.loadtxt(path + '/res.txt',delimiter = '\t', dtype='float')
-    print(alsoData)
-   # data = pd.read_csv(path + '/res.txt', sep="\t", header=None)  
-    #MSE_xGT = data[1]  
-    #MSE_xGT = MSE_xGT[1:]
-    #MSE_xGT = MSE_xGT.to_numpy()
-    #MSE_xGT = MSE_xGT.astype(float)
-
-    #MSE_AVG = data[6]
-   # MSE_AVG = MSE_AVG[1:]
-   # MSE_AVG = MSE_AVG.to_numpy()
-  #  MSE_AVG = MSE_AVG.astype(float) 
-   # MSE_AVG[0] = 0.0 
+    MSE_AVG = data[6]
+    MSE_AVG = MSE_AVG[1:]
+    MSE_AVG = MSE_AVG.to_numpy()
+    MSE_AVG = MSE_AVG.astype(float) 
+    MSE_AVG[0] = 0.0 
  
     
-   # plt.plot(np.arange(100),MSE_xGT, label = 'MSE: x-x_GT')
-   # plt.plot(np.arange(100),MSE_AVG, label = 'MSE: Avgeraging')  
+    plt.plot(np.arange(100),MSE_xGT, label = 'MSE: x-x_GT')
+    plt.plot(np.arange(100),MSE_AVG, label = 'MSE: Avgeraging')  
 
     plt.savefig(path + '/ResultGraph.png') 
+
+
+PlotRes("./results/20200716-191431Batch_Trials/noise_20200716-191431")
